613d3bec4cc4eb1bcf5979ddb9916b2b923916aa
bellemma <bell.e@northeastern.edu>
2022-12-08 20:17:13 -0500

    login

---
+++ controllers/songs/song-controller.js
@@ -0,0 +1,10 @@
+export default (app) => {
+  app.post('/api/tuits', createTuit);
+  app.get('/api/tuits', findTuits);
+  app.put('/api/tuits/:tid', updateTuit);
+  app.delete('/api/tuits/:tid', deleteTuit);
+
+}
+
+const findTuits = (req, res) =>
+    res.json(tuits);
